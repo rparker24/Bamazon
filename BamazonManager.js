16ee9390b5169b487466a925c121c277fa2aa5aa
rparker24 <robert@robertmparker.net>
2016-05-01 19:06:06 -0400

    setup functions for manager menu options

--- BamazonManager.js
+++ BamazonManager.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
 
+// settings for mysql DB connection
 var connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
@@ -8,19 +9,70 @@ var connection = mysql.createConnection({
   database : 'bamazon'
 });
 
+// settings for prompt
 var schema = {
   properties: {
-    name: {
-      message: 'Please select one of the following options: ',
+    number: {
+      message: 'Please select a menu option by number: ',
+      type: integer,
       required: true
-    },
-    password: {
-      hidden: true
     }
   }
 };
 
-prompt.start();
+function showOptions() {
+	var menuOptions = ['1. View Products for Sale', '2. View Low Inventory', '3. Add to Inventory', '4. Add New Product'];
+	console.log('Menu Options:');
+	for(var i = 0; i < menuOptions.length; i++) {
+		console.log(menuOptions[i]);
+	}
+}
 
-prompt.get(schema, function (err, result) {
-});
+showOptions();
+
+function runPrompt() {
+	prompt.start();
+	prompt.get(schema, function (err, result) {
+	});
+	checkSelected(result);
+}
+
+function checkSelected(option) {
+	switch(option) {
+		case 1:
+			console.log(1);
+			listProducts();
+			break;
+		case 2:
+			console.log(2);
+			showLowInventory();
+			break;
+		case 3:
+			console.log(3);
+			break;
+		case 4:
+			console.log(4);
+			break;
+		default:
+			console.log('You did not enter a number for a menu option, please try again');
+	}
+}
+
+// displays available products with their stats, runs if option 1 is selected
+function listProducts() {
+	connection.query('SELECT ItemID, ProductName, Price, StockQuantity FROM products', function(err, rows, fields) {
+		console.log('Available products for sale:');
+		for(var i = 0; i < rows.length; i++) {
+		  console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price + '   Stock: ' + rows[i].StockQuantity);
+		}
+	});
+}
+
+function showLowInventory() {
+	connection.query('SELECT ItemID, ProductName, Price, StockQuantity FROM products WHERE StockQuantity < 5', function(err, rows, fields) {
+		console.log('Items with low inventory:');
+		for(var i = 0; i < rows.length; i++) {
+		  console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price + '   Stock: ' + rows[i].StockQuantity);
+		}
+	});
+}
