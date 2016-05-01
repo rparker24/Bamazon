16bcd4e6e67e09a1989340d5ccb8831b59d14644
rparker24 <robert@robertmparker.net>
2016-05-01 19:31:10 -0400

    options 1 and 2 working

--- BamazonManager.js
+++ BamazonManager.js
@@ -9,48 +9,48 @@ var connection = mysql.createConnection({
   database : 'bamazon'
 });
 
-// settings for prompt
-var schema = {
-  properties: {
-    number: {
-      message: 'Please select a menu option by number: ',
-      type: integer,
-      required: true
-    }
-  }
-};
-
 function showOptions() {
 	var menuOptions = ['1. View Products for Sale', '2. View Low Inventory', '3. Add to Inventory', '4. Add New Product'];
 	console.log('Menu Options:');
 	for(var i = 0; i < menuOptions.length; i++) {
 		console.log(menuOptions[i]);
 	}
+	menuPrompt();
 }
 
-showOptions();
+function menuPrompt() {
 
-function runPrompt() {
+	// settings for prompt
+	var schema = {
+	  properties: {
+	    number: {
+	      message: 'Please select a menu option by number',
+	      required: true
+	    }
+	  }
+	};
+
 	prompt.start();
 	prompt.get(schema, function (err, result) {
+		var num = result.number;
+		checkSelected(num);
 	});
-	checkSelected(result);
 }
 
 function checkSelected(option) {
 	switch(option) {
-		case 1:
+		case '1':
 			console.log(1);
 			listProducts();
 			break;
-		case 2:
+		case '2':
 			console.log(2);
 			showLowInventory();
 			break;
-		case 3:
+		case '3':
 			console.log(3);
 			break;
-		case 4:
+		case '4':
 			console.log(4);
 			break;
 		default:
@@ -61,18 +61,26 @@ function checkSelected(option) {
 // displays available products with their stats, runs if option 1 is selected
 function listProducts() {
 	connection.query('SELECT ItemID, ProductName, Price, StockQuantity FROM products', function(err, rows, fields) {
+		if(err) throw err;
 		console.log('Available products for sale:');
-		for(var i = 0; i < rows.length; i++) {
-		  console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price + '   Stock: ' + rows[i].StockQuantity);
-		}
+		showSelectedProducts(rows);
 	});
+	connection.end();
 }
 
 function showLowInventory() {
 	connection.query('SELECT ItemID, ProductName, Price, StockQuantity FROM products WHERE StockQuantity < 5', function(err, rows, fields) {
+		if(err) throw err;
 		console.log('Items with low inventory:');
-		for(var i = 0; i < rows.length; i++) {
-		  console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price + '   Stock: ' + rows[i].StockQuantity);
-		}
+		showSelectedProducts(rows);
 	});
+	connection.end();
 }
+
+function showSelectedProducts(rows) {
+	for(var i = 0; i < rows.length; i++) {
+	  console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price + '   Stock: ' + rows[i].StockQuantity);
+	}
+}
+
+showOptions();
